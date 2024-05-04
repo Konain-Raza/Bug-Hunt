f55db151cb614c7bf300fec0ad9e74cf79ae42b4
Konain <konainraza105@gmail.com>
2024-05-04 19:40:24 +0500

    fixed hamberger button bug

--- script.js
+++ script.js
@@ -2,40 +2,28 @@
 
 
 let hamberger = document.querySelector(".hamberger");
-
 let mobileNav = document.querySelector(".nav-list");
- // Set initial display to block
-
-
 let bars = document.querySelectorAll(".hamberger span");
-
-let isActive = false;
-
+var isActive = true;
+var isActive = false; 
 hamberger.addEventListener("click", () => {
-isActive=true;
-
-mobileNav.classList.toggle("open");
-if (!isActive) {
-
-bars[0].style.transform = "rotate(-45deg)";
-
-bars[1].style.opacity = "0";
-
-bars[2].style.transform = "rotate(45deg)";
-
-isActive = true;
-} else {
+  if (isActive) {
+    mobileNav.classList.remove("open");
+    mobileNav.style.display = "none"; 
+    bars[0].style.transform = "rotate(0deg)";
+    bars[1].style.opacity = "1";
+    bars[2].style.transform = "rotate(0deg)";
+    isActive = false;
+  } else {
+    mobileNav.classList.add("open");
     mobileNav.style.display = "block";
-
-bars[0].style.transform = "rotate(0deg)";
-
-bars[1].style.opacity = "1";
-
-bars[2].style.transform = "rotate(0deg)";
-
-isActive = false;
-}
+    bars[0].style.transform = "rotate(-45deg)";
+    bars[1].style.opacity = "0";
+    bars[2].style.transform = "rotate(45deg)";
+    isActive = true;
+  }
 });
+
 
 window.addEventListener("load", function () {
     addRequiredClasses();
